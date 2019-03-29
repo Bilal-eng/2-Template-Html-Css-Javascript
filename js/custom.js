b2b0445d38a5a1d8ac9fc6708bf4833b49cf7101
Bilal Sheikh Khalil <bilalsheikhkhalil@gmail.com>
2019-03-30 00:54:52 +0300

    add a footer section

--- js/custom.js
+++ js/custom.js
@@ -63,5 +63,45 @@ $(function(){
 
     });
 
+    // Our Auto Slider Code
+
+    (function autoSlider() {
+
+        $('.slider .active').each(function(){
+
+            if(!$(this).is(':last-child')) {
+
+                $(this).delay(3000).fadeOut(1000, function(){
+
+                    $(this).removeClass('active').next().addClass('active').fadeIn();
+
+                    autoSlider();
+                });
+            
+            } else {
+
+                $(this).delay(3000).fadeOut(1000, function(){
+
+                    $(this).removeClass('active');
+
+                    $('.slider div').eq(0).addClass('active').fadeIn();
+
+                    autoSlider();
+                });
+            }
+
+        });
+
+    }());
+
+    // Trigger MixitUp
+
+    window.mixitup('#Container');
+
+    $('.shuffle li').click(function() {
+
+        $(this).addClass('selected').siblings().removeClass('selected');
+
+    });
 
 });
